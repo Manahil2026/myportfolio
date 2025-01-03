146d8a7fe03369b7923147d26b18808f8774fefa
Manahil2026 <manahil.im111@gmail.com>
2025-01-03 13:13:16 -0500

    create index file in data folder for better readability

---
+++ data/index.ts
@@ -0,0 +1,6 @@
+export const GridItems = [
+    {title: 'title1', description: 'Desc1', id: 1 },
+    {title: 'title1', description: 'Desc1', id: 1 },
+    {title: 'title1', description: 'Desc1', id: 1 },
+    {title: 'title1', description: 'Desc1', id: 1 },
+   ]
