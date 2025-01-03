e7eb20eb16005b79c1954fa4b4e160aab59d9d3b
Manahil2026 <manahil.im111@gmail.com>
2025-01-03 13:13:36 -0500

    edit margin

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -17,7 +17,7 @@ const Hero = () => {
            
         </div>
 
-        <div className='flex justify-center relative my-20 z-10'>
+        <div className='flex justify-center relative my-10 z-10'>
             <div className='max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center'>
                 <TextGenerateEffect 
                     className='text-center text-[40px] md:text-5xl lg:text-6xl' 
